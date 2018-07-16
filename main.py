09c992f83e2cae296d1f059514cc35561aba9f59
darren-2016 <darren.j.draper@gmail.com>
2018-07-16 16:24:31 +1000

    Commentary update.

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ import serial
 import serial.tools.list_ports
 
 
-####################
+########################################
 # OBDEmulator class
 #
 class OBDEmulator:
@@ -17,7 +17,7 @@ class OBDEmulator:
   def __init__(self, verbose=True):
     self.verbose = verbose
 
-  ####################
+  ########################################
   # scanSerialPorts
   def scanSerialPorts(self):
     #portlist = serial.tools.list_ports
@@ -35,7 +35,7 @@ class OBDEmulator:
       print("{} OBD-II devices found".format(devicesFound))
       return False
 
-  ####################
+  ########################################
   # connectDevice
   def connectDevice(self):
     i, portPath = self.scanSerialPorts()
@@ -43,12 +43,12 @@ class OBDEmulator:
       print "Connecting"
       self.serialPort = serial.Serial(portPath, baudrate=self.baudRate, timeout=1)
 
-  ####################
+  ########################################
   # closeConnection
   def closeConnection(self):
     self.serialPort.close()
 
-  ####################
+  ########################################
   # sendCommand
   def sendCommand(self, command):
     self.serialPort.write((command + '\r'))
@@ -65,56 +65,80 @@ class OBDEmulator:
     out = self.serialPort.readline()
     print '<<' + out
 
-  ##############################
-  # setEngineRPM
-  # PID: 010C
-  # rpm : rpm
+  ########################################
+  # Function:    setEngineRPM
+  # Parameters:  rpm (rpm)
+  # Description: PID=010C
   def setEngineRPM(self, rpm):
       self.sendCommand("ATSET 010C=" + str(rpm))
 
-  ##############################
-  # setVehicleSpeed
-  # PID: 010D
-  # speed : km/h
+  ########################################
+  # Function:    setVehicleSpeed
+  # Parameters:  speed (km/h)
+  # Description: PID=010D
   def setVehicleSpeed(self, speed):
     self.sendCommand("ATSET 010D=" + str(speed))
 
-  ##############################
-  # setThrottlePosition
-  # PID: 0111
-  # throttle : 0 - 100%
+  ########################################
+  # Function:    setThrottlePosition
+  # Parameters:  throttle (0 - 100%)
+  # Description: PID=0111
   def setThrottlePosition(self, throttle):
     self.sendCommand("ATSET 0111=" + str(throttle))
   
-  ##############################
-  # setFuelLevelInput
-  # PID: 012F
-  # fuelLevel : 0 - 100%
+  ########################################
+  # Function:    setFuelLevelInput
+  # Parameters:  fuelLevel (0 - 100%)
+  # Description: PID=012F
   def setFuelLevelInput(self, fuelLevel):
     self.sendCommand("ATSET 012F=" + str(fuelLevel))
   
-  ##############################
-  # setEngineFuelRate
-  # PID: 015E
-  # rate: L/h
+  ########################################
+  # Function:    setEngineFuelRate
+  # Parameters:  rate (L/h)
+  # Description: PID=015E
   def setEngineFuelRate(self, rate):
     self.sendCommand("ATSET 015E=" + str(rate))
   
-  ##############################
-  # setMAFAirFlowRate
-  # PID: 0110
-  # rate : g/s
+  def getEngineFuelRate(self):
+    self.sendCommand("ATGET 015E")
+    out = self.serialPort.readline()
+    print "<<<<< " + str(out)
+    out = self.serialPort.readline()
+    print "<<<<< " + str(out)
+    out = self.serialPort.readline()
+    print "<<<<< " + str(out)
+    out = self.serialPort.readline()
+    print "<<<<< " + str(out)
+    out = self.serialPort.readline()
+    print "<<<<< " + str(out)
+    out = self.serialPort.readline()
+    print "<<<<< " + str(out)
+    out = self.serialPort.readline()
+    print "<<<<< " + str(out)
+    out = self.serialPort.readline()
+    print "<<<<< " + str(out)
+    out = self.serialPort.readline()
+    print "<<<<< " + str(out)
+    
+  ########################################
+  # Function:    setMAFAirFlowRate
+  # Parameters:  rate (g/s)
+  # Description: PID=0110
   def setMAFAirFlowRate(self, rate):
     self.sendCommand("ATSET 0110=" + str(rate))
 
-  ##############################
-  # setCalculatedEngineLoad
-  # PID: 0104
-  # rate : %
+  ########################################
+  # Function:    setCalculatedEngineLoad
+  # Parameters:  rate (%)
+  # Description: PID=0104
   def setCalculatedEngineLoad(self, load):
     self.sendCommand("ATSET 0104=" + str(load))
 
 
+########################################
+# Function:     main
+# Description:  Main function.
 def main():
   obd = OBDEmulator()
 
@@ -131,6 +155,7 @@ def main():
   obd.setThrottlePosition(19)
   obd.setFuelLevelInput(66)
   obd.setEngineFuelRate(111)
+  obd.getEngineFuelRate()
   obd.setMAFAirFlowRate(99)
   obd.setCalculatedEngineLoad(48)
 
